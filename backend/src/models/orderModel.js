f354b67ad27aad01b4bf03779f11dbbd789ad12e
anil <anil.anilkumarchanvan76@gmail.com>
2026-03-08 17:05:41 +0530

    feat: implement user schema

---
+++ backend/src/models/orderModel.js
@@ -0,0 +1,40 @@
+const mongoose = require("mongoose");
+
+const orderSchema = new mongoose.Schema(
+{
+  items: [
+    {
+      product: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Product",
+        required: true
+      },
+      quantity: {
+        type: Number,
+        required: true
+      },
+      price: {
+        type: Number,
+        required: true
+      }
+    }
+  ],
+
+  totalAmount: {
+    type: Number,
+    required: true
+  },
+
+  paymentMethod: {
+    type: String,
+    enum: ["cash", "card", "upi"],
+    default: "cash"
+  }
+
+},
+{
+  timestamps: true
+}
+);
+
+module.exports = mongoose.model("Order", orderSchema);
